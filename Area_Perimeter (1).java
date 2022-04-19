4eda1fdc80d80138e2b6e0b8291501950296f1da
Sahana-777 <103114574+Sahana-777@users.noreply.github.com>
2022-04-19 12:08:28 +0530

    Add files via upload

---
+++ Area_Perimeter (1).java
@@ -0,0 +1,14 @@
+class Area_Perimeter
+  {
+    public static void main(String args[])
+    {
+      int l,b,area,perimeter;
+      l=7;
+      b=5;
+      area=l*b;
+      perimeter=2*l+2*b;
+      System.out.println(area);
+      System.out.println(perimeter);
+      
+    }
+  }
