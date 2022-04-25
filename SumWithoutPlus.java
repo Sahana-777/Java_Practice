4c74aac9fa4bd71f4fb29c7e8f6f110dcfe3913b
sahanapandraka <103114574+SahanaPandraka@users.noreply.github.com>
2022-04-25 15:48:38 +0530

    Add files via upload

---
+++ SumWithoutPlus.java
@@ -0,0 +1,35 @@
+/*  JFM1T2_Assignment5:
+
+    Write a program to print the sum of two numbers without using + operator.
+    Prompt the user input from the terminal.
+    
+    Sample Input:
+    Enter first number: 
+    22
+    Enter second number: 
+    50
+    
+    Expected Output:
+    The sum of two numbers is: 72
+*/
+
+import java.util.Scanner;
+class SumWithoutPlus
+  {
+     public static void main(String[] arg) 
+	 {
+	   int x, y ;
+	   Scanner in = new Scanner(System.in);	
+	   System.out.print("Input first number: ");
+	   x = in.nextInt(); 
+	   System.out.print("Input second number: ");
+	   y = in.nextInt(); 
+      while(y != 0){
+            int carry = x & y;
+            x = x ^ y;
+            y = carry << 1;
+        }
+        System.out.print("Sum: "+x); 
+		System.out.print("\n");     	
+	}	
+}
